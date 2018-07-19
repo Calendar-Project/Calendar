a03099e3b42c2e13b3426e3e013259476a450759
ElifHangul <elifhangul96@gmail.com>
2018-07-19 18:07:57 +0300

    Connecting database with Grape Api to communicate via json

--- config/application.rb
+++ config/application.rb
@@ -1,4 +1,4 @@
-require_relative 'boot'
+require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
 
@@ -9,7 +9,8 @@ Bundler.require(*Rails.groups)
 module Calendar
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 5.2
+    config.paths.add File.join('app', 'api'), glob: File.join('**', '*.rb')
+    config.autoload_paths += Dir[Rails.root.join('app', 'api', '*')]
     config.beginning_of_week=:sunday
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
