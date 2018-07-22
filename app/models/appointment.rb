d36baa385b0346e909f9b371dc4acd483c80801a
ElifHangul <elifhangul96@gmail.com>
2018-07-22 15:34:33 +0300

    Adding validation for input

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -1,2 +1,7 @@
 class Appointment < ApplicationRecord
+  validates :title, presence: true
+  validates :start_date, presence: true
+  validates :start_time, presence: true
+  validates :end_date, presence: true
+  validates :end_time, presence: true
 end
