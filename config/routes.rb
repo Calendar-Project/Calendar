08a419f8a2e0c64ba5291916d6974d9ba9135d56
melissceylan <melisceylan49@gmail.com>
2018-08-06 22:54:07 +0300

    New appointment page is designed

--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,6 @@ Rails.application.routes.draw do
   get 'welcome/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :appointments do
-<<<<<<< HEAD
-
-=======
->>>>>>> cca1395ba6ca82511346e11a44cc0aa127a394e1
     member do
       get 'repeat_weekly'
       get 'repeat_monthly'
@@ -15,11 +11,6 @@ Rails.application.routes.draw do
     end
   end
 
-<<<<<<< HEAD
-
-
-=======
->>>>>>> cca1395ba6ca82511346e11a44cc0aa127a394e1
   root 'welcome#index'
 
 
